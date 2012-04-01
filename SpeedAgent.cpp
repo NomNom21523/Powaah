b49be5eb6c0732e02b656258e8ce6d6a1bc29c8c
Tim Uusitalo <tim@uusitalo.cc>
2012-04-01 21:25:01 +0200

    added todays work

--- SpeedAgent.cpp
+++ SpeedAgent.cpp
@@ -12,6 +12,16 @@ SpeedAgent::~SpeedAgent()
 
 void SpeedAgent::update(CarState &carState, const float dt)
 {
+    // find the sensor that has the longest length
+    int longestTrackSensor = 0;
+    for (int i = 0; i < TRACK_SENSORS_NUM; i++) {
+        if (carState.getTrack(i) > carState.getTrack(longestTrackSensor)) {
+            longestTrackSensor = i;
+        }
+    }
+
+    // put a charge at the longest sensor position
+
 
 }
 
